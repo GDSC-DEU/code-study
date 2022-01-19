28f5b4c8fbc9e03651fe27e6f850527e72f74cb2
Dong-Young Kim <31337.persona@gmail.com>
2022-01-19 20:34:34 +0900

    style: remove `print`

--- pers0n4/42577.py
+++ pers0n4/42577.py
@@ -7,7 +7,6 @@ def solution(phone_book: list[str]):
         # phone의 서브 스트링이 해시에 있는지 확인
         for number in phone[:-1]:
             key += number
-            print(key)
             if hash_table.get(key, False):
                 return False
     return True

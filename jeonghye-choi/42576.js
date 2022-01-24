ef85ba5585fd573fd145101a95d0db95901f1f58
jeonghye-choi <ppllhm.0@gmail.com>
2022-01-24 23:35:25 +0900

    fix(programmers): edit `42576.js`

--- jeonghye-choi/42576.js
+++ jeonghye-choi/42576.js
@@ -1,7 +1,17 @@
+function hashing(completion) {
+  let hashTable = {};
+  for (let person of completion) {
+    if (hashTable[person]) hashTable[person] += 1;
+    else hashTable[person] = 1;
+  }
+  return hashTable;
+}
+
 function solution(participant, completion) {
+  let hashTable = hashing(completion);
+
   for (let person of participant) {
-    if (completion.includes(person)) {
-      completion.splice(completion.indexOf(person), 1);
-    } else return person;
+    if (!hashTable[person] || hashTable[person] === 0) return person;
+    else hashTable[person] -= 1;
   }
 }

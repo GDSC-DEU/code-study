1e297895a089404fc27712b4705a53d89f6c6160
sudhdkso <waga1009@gmail.com>
2022-02-08 19:29:50 +0900

    42583/42584

--- sudhdkso/42583.cpp
+++ sudhdkso/42583.cpp
@@ -6,17 +6,16 @@ using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
 	int answer = 0, count = 0;
-	int sum = 0; //다리위에 있는 트럭 무게의 총 합
+	int sum = 0;
 	queue<int> q;
 	while (1) {
 
 		//cout << count << ":" << truck_weights.size() << " ";
 
-		if (count > truck_weights.size() - 1) //모든 트럭이 다 다리위에 올라갔을 때 
+		if (count > truck_weights.size() - 1)
 			break;
-        //다리위에 있는 트럭의 무게의 총 합과 그 다음 기다리고 있는 트럭의 무게가 제한 무게보다 작은 경우
-		if (sum + truck_weights[count] <= weight) { 
-			if (q.size() < bridge_length) { //다리길이와 다리에 올라가있는 트럭의 수 비교
+		if (sum + truck_weights[count] <= weight) {
+			if (q.size() < bridge_length) {
 				sum += truck_weights[count];
 				q.push(truck_weights[count]); count++;
 				answer++;
